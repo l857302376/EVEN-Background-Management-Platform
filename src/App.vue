0dfac673159ded86b35d7284c5def0db87cce741
unknown <ylia0098@student.monash.edu>
2024-03-29 14:58:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h2>我是app</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = 'asd'
+</script>
+
+<style scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
